9368c9a07f0d4be5fdaca3f94532430585453c97
Diego Barrios Romero <eldruin@gmail.com>
2021-05-13 09:15:06 +0200

    Improve benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,18 +1,7 @@
-use criterion::{
-    black_box, criterion_group, criterion_main, Bencher, Criterion, ParameterizedBenchmark,
-    Throughput,
-};
+use core::hash::Hasher;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use rand::{rngs::SmallRng, FromEntropy, Rng};
-use smbus_pec::pec;
-
-fn do_pec_bench(b: &mut Bencher, size: &usize) {
-    let mut v: Vec<u8> = Vec::with_capacity(*size);
-    fill_random(&mut v);
-    b.iter(|| {
-        let pec = pec(&v);
-        black_box(&pec);
-    });
-}
+use smbus_pec::{pec, Pec};
 
 fn fill_random(v: &mut Vec<u8>) {
     let mut rng = SmallRng::from_entropy();
@@ -22,14 +11,28 @@ fn fill_random(v: &mut Vec<u8>) {
 }
 
 const MESSAGE_SIZES: [usize; 7] = [1, 3, 5, 20, 50, 100, 500];
-
-fn benchmarks(byte_sizes: &[usize]) -> ParameterizedBenchmark<usize> {
-    ParameterizedBenchmark::new("pec", do_pec_bench, byte_sizes.iter().cloned())
-        .throughput(|s| Throughput::Bytes(*s as u64))
-}
-
 fn bench(c: &mut Criterion) {
-    c.bench("bench", benchmarks(&MESSAGE_SIZES[..]));
+    let mut group = c.benchmark_group("pec");
+    for size in &MESSAGE_SIZES {
+        group.throughput(Throughput::Bytes(*size as u64));
+        let mut data: Vec<u8> = Vec::with_capacity(*size);
+        fill_random(&mut data);
+        group.bench_with_input(BenchmarkId::new("fn", *size), &data, |b, data| {
+            b.iter(|| {
+                let pec = pec(data);
+                black_box(&pec);
+            });
+        });
+        group.bench_with_input(BenchmarkId::new("hasher", *size), &data, |b, data| {
+            b.iter(|| {
+                let mut hasher = Pec::new();
+                hasher.write(data);
+                let pec = hasher.finish();
+                black_box(&pec);
+            });
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(benches, bench);
