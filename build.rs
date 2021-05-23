876f0e645b6d50d77a2374bc29519921634c06a1
Diego Barrios Romero <eldruin@gmail.com>
2021-05-23 23:00:06 +0200

    Avoid unnecessary recompilations

--- build.rs
+++ build.rs
@@ -4,8 +4,5 @@ crc8!(fn pec, 7, 0, "");
 build_rs_lookup_table_file_generation!(fn write_file, pec, LOOKUP_TABLE, "lookup_table.rs", u8, 256);
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=lib.rs");
-
     write_file().expect("Couldn't write lookup table file!");
 }
